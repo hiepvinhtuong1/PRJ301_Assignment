1c112e97b009a2a08fc15463c2b3c818603a62cd
= <=>
2025-02-23 11:49:24 +0700

    git leave-request and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'; // Sử dụng Navigate để chuyển hướng
+import Navbar from './component/common/Navbar';
+import LeaveRequestPage from './component/leave_request/LeaveRequestPage';
+import LoginPage from './component/auth/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        {/* Hiển thị Navbar cho tất cả các trang trừ /login */}
+        <Routes>
+          {/* Nếu đang ở trang login, không hiển thị Navbar */}
+          <Route path="/login" element={<LoginPage />} />
+          {/* Các route khác, ví dụ: */}
+          {/* <Route path="/profile" element={<ProtectedRoute element={<ProfilePage />} />} /> */}
+          <Route path="/leave-request" element={<><Navbar /><LeaveRequestPage /></>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
