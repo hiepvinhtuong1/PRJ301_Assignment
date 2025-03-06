a493095563240011d3392ae1e24bc9d38c56257a
hiepvinhtuong1 <buituanhiepvinhtuong@gmail.com>
2025-03-06 15:30:30 +0700

    update page

--- backend/src/main/java/com/hipdev/LeaveManagement/service/LeaveRequestService.java
+++ backend/src/main/java/com/hipdev/LeaveManagement/service/LeaveRequestService.java
@@ -2,13 +2,14 @@ package com.hipdev.LeaveManagement.service;
 
 import com.hipdev.LeaveManagement.dto.LeaveRequestDTO;
 import com.hipdev.LeaveManagement.dto.request.leave_request.CreateLeaveRequest;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface LeaveRequestService {
     public List<LeaveRequestDTO> getAllLeaveRequests();
     public LeaveRequestDTO getLeaveRequestById(Long id);
-    public List<LeaveRequestDTO> getLeaveRequestByUsername();
+    public Page<LeaveRequestDTO> getLeaveRequestByUsername(int page, int size);
     public LeaveRequestDTO createLeaveRequest(CreateLeaveRequest request);
     public LeaveRequestDTO updateLeaveRequest(LeaveRequestDTO leaveRequestDTO);
     public Void deleteLeaveRequest(Long leaveRequestId);
